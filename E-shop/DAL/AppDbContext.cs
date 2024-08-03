2b49c9bf0d1ac366dee1561c61dfa054ee25c191
Atharv Mankar <137928350+AtharvM66@users.noreply.github.com>
2024-08-03 14:58:38 +0530

    Add files via upload

---
+++ E-shop/DAL/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using CRUDwithRazorPages.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CRUDwithRazorPages.DAL
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+        public virtual DbSet<Product> Products { get; set; }
+    }
+}
